0b6b2d3e259e244efba4e17c99fff805233e5f6f
Carlos Soria <dionisos.noise@gmail.com>
2018-04-17 12:09:09 -0500

    Inicial Github

---
+++ introduccion-1.py
@@ -0,0 +1,23 @@
+"""
+
+Python fue inventando en 1991 por el programador holandés Guido Van Rossum
+Suele considerarse un "lenguaje prototipo", una vez diseñada una solución
+o algoritmo se suele pasar a C/C++ si es que se desea más performance ...
+
+Existen varias caracteristicas que lo diferencian de otros lenguajes, entre ellas:
+- El manejo númerico es impresionante, considerando matrices y números complejos p.e.
+- Se dispone de "enteros infinitos" es decir no hay límite (sólo por hardware)
+- No existen bloques de código ni sentencias begin/end todo es por indentación!
+- Las variables no son declaradas, se crean al asignarles un valor ...
+- Python trata cada variable como una referencia!
+
+"""
+
+a, b, c = -100, 123, 456 # Asignación en Tupla
+numero = ((10 - 5) * 3) ** 4
+print (numero)
+
+"""
+PENDIENTE
+3. Your First Programs
+"""
